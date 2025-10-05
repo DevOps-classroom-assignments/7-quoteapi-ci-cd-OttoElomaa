2e2d43c29c4df1249786cf444cc526a3dc3bdb47
Otto Elomaa <otto.elomaa99@gmail.com>
2025-10-06 00:01:59 +0300

    Commit purely to test that it does not go to Render (turned off auto-deploy)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/quote',(req,res)=>{
     const randomIndex = Math.floor(Math.random() * quotes.length);
-    res.json({quote: quotes[randomIndex], edit: "I edited this file"});
+    res.json({quote: quotes[randomIndex], version_message: "This version should not be visible in Render"});
 });
 
 const PORT = process.env.PORT || 3000;
